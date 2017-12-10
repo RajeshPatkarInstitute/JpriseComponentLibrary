b158cd0316ae7fc7edd8693fba1c8d00563182ae
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-12-10 14:29:42 +0530

    Update AStack.java

--- DataStructures/Stack/AStack/src/AStack.java
+++ DataStructures/Stack/AStack/src/AStack.java
@@ -1,1 +1,37 @@
+package com.rajeshpatkar.components.datastructures.stack;
+public class AStack {
 
+    private int[] stk = new int[10];
+    private int sp = 10;
+
+    public void push(int v) {
+        if (sp == 0) {
+            System.out.println("Stack Overflow");
+        } else {
+            sp = sp - 1;
+            stk[sp] = v;
+        }
+    }
+
+    public int pop() {
+        int temp = -1;
+        if (sp == 10) {
+            System.out.println("Stack Underflow");
+        } else {
+            temp = stk[sp];
+            sp = sp + 1;
+        }
+        return temp;
+    }
+
+    public void print() {
+        System.out.println("Printing AStack");
+        if (sp == 10) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+        for (int i = sp; i < 10; i++) {
+            System.out.println(stk[i]);
+        }
+    }
+}
