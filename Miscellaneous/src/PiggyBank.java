d53c42f08e0bc47f7f2a5f456468c5c20b715fa9
Rajesh Patkar <patkar@rajeshpatkar.com>
2017-12-10 14:19:35 +0530

    Update PiggyBank.java

--- Miscellaneous/src/PiggyBank.java
+++ Miscellaneous/src/PiggyBank.java
@@ -1,1 +1,35 @@
+package com.rajeshpatkar.components.miscellaneous;
+public class PiggyBank{
+    private String name="";
+    private int balance;
+    private int lt;
+    public PiggyBank(){
+    }
+    public PiggyBank(String name,int amount){
+        this.name = name;
+        this.balance = amount;
+        lt = amount;
+    }
+    public PiggyBank(String name){
+        this(name,300);
+    }
+    public void deposit(int v) {
+        balance = balance + v;
+        lt = v;
+    }
 
+    public void withdraw(int v) {
+        if (balance >= v) {
+            balance = balance - v;
+            lt = -v;
+        } else {
+            System.out.println("Cannot Withdraw more than the Balance");
+        }
+    }
+
+    public void statement() {
+        System.out.println("Acount Name "+name);
+        System.out.println("Balance = " + balance
+                + "\n" + "Last Transaction = " + lt);
+    }
+}
