55b667075a4c89e9926e31e3073a5318a6a7c700
saeedd77 <33974679+saeedd77@users.noreply.github.com>
2017-12-16 01:32:35 +0530

    Update PiggyBank.java
    Added getters and setters to test the results

--- Miscellaneous/src/PiggyBank.java
+++ Miscellaneous/src/PiggyBank.java
@@ -1,23 +1,56 @@
 package com.rajeshpatkar.components.miscellaneous;
+
 public class PiggyBank{
-    private String name="";
+
+	private String name="";
     private int balance;
     private int lt;
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getLt() {
+		return lt;
+	}
+
+	public void setLt(int lt) {
+		this.lt = lt;
+	}
+
+	//Constructors
     public PiggyBank(){
     }
+    
     public PiggyBank(String name,int amount){
         this.name = name;
         this.balance = amount;
         lt = amount;
     }
+    
     public PiggyBank(String name){
         this(name,300);
     }
+    
+    //Deposit
     public void deposit(int v) {
         balance = balance + v;
         lt = v;
     }
 
+    //Withdraw
     public void withdraw(int v) {
         if (balance >= v) {
             balance = balance - v;
@@ -27,6 +60,7 @@ public class PiggyBank{
         }
     }
 
+    //Statement
     public void statement() {
         System.out.println("Acount Name "+name);
         System.out.println("Balance = " + balance
