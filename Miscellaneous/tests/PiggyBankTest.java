9b846d51b6c20ea9321cc3f87f5bbb8fd0381e7a
saeedd77 <33974679+saeedd77@users.noreply.github.com>
2017-12-16 01:29:38 +0530

    Update PiggyBankTest.java

--- Miscellaneous/tests/PiggyBankTest.java
+++ Miscellaneous/tests/PiggyBankTest.java
@@ -1,1 +1,53 @@
 
+package com.rajeshpatkar.components.miscellaneous.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.rajeshpatkar.components.miscellaneous.PiggyBank;
+
+public class PiggyBankTest {
+ PiggyBank piggyBank;
+	
+	@Test
+	public void testPiggyBankStringInt() {
+		
+		PiggyBank piggyBank = new PiggyBank("User1", 5000);
+		assertEquals("User1", piggyBank.getName());
+		assertEquals(5000, piggyBank.getBalance());
+	}
+
+	@Test
+	public void testPiggyBankString() {
+		PiggyBank piggyBank = new PiggyBank("User2");
+		assertEquals("User2", piggyBank.getName());
+		assertEquals(300, piggyBank.getBalance());
+	}
+
+// For each test cases run before test	
+	@Before
+	public void setUp(){
+		piggyBank = new PiggyBank("User1", 5000);	
+	}
+	
+	@Test
+	public void testDeposit() {
+	
+		piggyBank.deposit(1000);
+		
+		assertEquals(6000,piggyBank.getBalance());
+	
+	}
+
+	@Test
+	public void testWithdraw() {
+
+		piggyBank.withdraw(1000);
+		
+		assertEquals(4000,piggyBank.getBalance());
+	
+	}
+
+}
